ed63c36a7e65a133560c051879dc859644ab0280
Pete Gordon <pete@petegordon.com>
2015-03-15 11:47:32 -0400

    Update the database to be specific about foreign key column name in relation to table name

--- example/desire_inserts.sql
+++ example/desire_inserts.sql
@@ -7,17 +7,17 @@ INSERT into my_desire(wants_something) VALUES('more of');
 INSERT into my_desire(wants_something) VALUES('time with');
 INSERT into my_desire(wants_something) VALUES('time at');
 
-INSERT INTO object_desire(object_id, desire_id)
+INSERT INTO object_desire(my_object_id, my_desire_id)
   SELECT my_object.id, my_desire.id FROM my_object, my_desire WHERE is_something='family' and wants_something='time with';
-INSERT INTO object_desire(object_id, desire_id)
+INSERT INTO object_desire(my_object_id, my_desire_id)
   SELECT my_object.id, my_desire.id FROM my_object, my_desire WHERE is_something='home' and wants_something='time at';
-INSERT INTO object_desire(object_id, desire_id)
+INSERT INTO object_desire(my_object_id, my_desire_id)
   SELECT my_object.id, my_desire.id FROM my_object, my_desire WHERE is_something='wisdom' and wants_something='more of';
-INSERT INTO object_desire(object_id, desire_id)
+INSERT INTO object_desire(my_object_id, my_desire_id)
   SELECT my_object.id, my_desire.id FROM my_object, my_desire WHERE is_something='wisdom' and wants_something='time with';
-INSERT INTO object_desire(object_id, desire_id)
+INSERT INTO object_desire(my_object_id, my_desire_id)
   SELECT my_object.id, my_desire.id FROM my_object, my_desire WHERE is_something='love' and wants_something='more of';
-INSERT INTO object_desire(object_id, desire_id)
+INSERT INTO object_desire(my_object_id, my_desire_id)
   SELECT my_object.id, my_desire.id FROM my_object, my_desire WHERE is_something='love' and wants_something='time with';
-INSERT INTO object_desire(object_id, desire_id)
+INSERT INTO object_desire(my_object_id, my_desire_id)
   SELECT my_object.id, my_desire.id FROM my_object, my_desire WHERE is_something='hope' and wants_something='more of';
